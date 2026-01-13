b3ea92f302a5a753be888e39810c944300996879
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-13 20:41:03 +0100

    refactor(devtools): replace formDevtoolsPlugin func with FormDevtoolsPanel component

--- src/app/ui/tanstack-devtools.tsx
+++ src/app/ui/tanstack-devtools.tsx
@@ -1,5 +1,5 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
+import { FormDevtoolsPanel } from "@tanstack/react-form-devtools";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
@@ -16,7 +16,10 @@ export function TanstackDevToolsIntegration() {
           name: "Tanstack Query",
           render: <ReactQueryDevtoolsPanel />,
         },
-        formDevtoolsPlugin(),
+        {
+          name: "Tanstack Form",
+          render: <FormDevtoolsPanel />,
+        },
       ]}
     />
   );
