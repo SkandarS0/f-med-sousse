124e3aa6e0bdbcffab1ebc05581be22d1ae98cba
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-14 13:30:03 +0100

    style(unauthenticated-layout): enhance structure and responsiveness

--- src/app/ui/layouts/unauthenticated.tsx
+++ src/app/ui/layouts/unauthenticated.tsx
@@ -2,20 +2,32 @@ import { Outlet } from "@tanstack/react-router";
 
 export function UnauthenticatedLayout() {
   return (
-    <>
+    <div className="flex flex-col h-dvh">
       <UnauthenticatedHeader />
-      <main>
-        <Outlet />
+      <main className="flex-1 min-h-0 overflow-y-auto">
+        <div className="flex min-h-full w-full max-w-sm items-center mx-auto p-6">
+          <div className="w-full">
+            <Outlet />
+          </div>
+        </div>
       </main>
       <UnauthenticatedFooter />
-    </>
+    </div>
   );
 }
 
 function UnauthenticatedHeader() {
-  return <header>Unauthenticated Header</header>;
+  return (
+    <header className="shrink-0 px-6 py-4 border-b">
+      Unauthenticated Header
+    </header>
+  );
 }
 
 function UnauthenticatedFooter() {
-  return <footer>Unauthenticated Footer</footer>;
+  return (
+    <footer className="shrink-0 px-6 py-4 border-t">
+      Unauthenticated Footer
+    </footer>
+  );
 }
