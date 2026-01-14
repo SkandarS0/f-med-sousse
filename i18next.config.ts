2a8927b016b8bd578f34fd12b07e748ce88091ff
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-14 23:17:53 +0100

    refactor(i18next-cli): reorder locales and remove unused language properties in the config file

--- i18next.config.ts
+++ i18next.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'i18next-cli';
 
 export default defineConfig({
-  locales: ['en', 'fr'],
+  locales: ['fr', 'en'],
   extract: {
     input: ['src/**/*.{js,jsx,ts,tsx}'],
     output: 'src/shared/i18n/{{language}}/{{namespace}}.ts',
     defaultNS: 'messages',
     outputFormat: 'ts',
-    primaryLanguage: 'fr',
-    secondaryLanguages: ['en']
   },
 });
