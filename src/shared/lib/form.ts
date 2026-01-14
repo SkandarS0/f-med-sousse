80623b5e4a5384d3e8d0d646c53449923ed3c65a
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-14 22:33:56 +0100

    feat(shared-lib): introduce tanstack form hooks with composition in mind

---
+++ src/shared/lib/form.ts
@@ -0,0 +1,21 @@
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+import { EmailFormField } from "@/shared/ui/form/email-field";
+import { PasswordFormField } from "@/shared/ui/form/password-field";
+import { SubmitButton } from "@/shared/ui/form/submit-button";
+
+const { fieldContext, formContext, useFieldContext, useFormContext } =
+  createFormHookContexts();
+
+export const { useAppForm } = createFormHook({
+  fieldComponents: {
+    PasswordInput: PasswordFormField,
+    EmailInput: EmailFormField,
+  },
+  formComponents: {
+    SubmitButton,
+  },
+  fieldContext,
+  formContext,
+});
+
+export { useFieldContext, useFormContext };
