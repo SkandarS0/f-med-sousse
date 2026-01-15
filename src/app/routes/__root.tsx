2b00dd8f64ec27de33c7bc9fe6711797c5c39841
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-15 21:51:15 +0100

    feat(routes): add HeadContent component to root route

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,5 +1,9 @@
 import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import {
+  createRootRouteWithContext,
+  HeadContent,
+  Outlet,
+} from "@tanstack/react-router";
 import { TanstackDevToolsIntegration } from "@/app/ui/tanstack-devtools";
 
 interface AppRouterContext {
@@ -9,6 +13,7 @@ interface AppRouterContext {
 export const Route = createRootRouteWithContext<AppRouterContext>()({
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanstackDevToolsIntegration />
     </>
