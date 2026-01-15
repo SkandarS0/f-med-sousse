7e3a2b2ef6ffee81a5c6cc700043c093b0fe0689
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-15 21:52:23 +0100

    feat(login): refactor LoginPage component and update route configuration

--- src/app/routes/(auth)/login.tsx
+++ src/app/routes/(auth)/login.tsx
@@ -1,8 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import z from "zod";
-import { LoginPage } from "@/pages/auth/login";
+import { LoginPage } from "@/pages/login";
+import { getRouteTitle } from "@/shared/routes/title";
 
 export const Route = createFileRoute("/(auth)/login")({
+  head: (ctx) => ({
+    meta: [
+      {
+        title: getRouteTitle(ctx.match.fullPath),
+      },
+    ],
+  }),
   validateSearch: z.object({
     redirectTo: z.string().optional(),
   }),
