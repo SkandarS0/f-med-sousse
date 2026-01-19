4684e4057eb7aa28ebe514fceccf0947e263d84c
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 23:38:02 +0100

    refactor(web/auth): update login schema to extend user schema

--- apps/web/src/features/auth/model/schema.ts
+++ apps/web/src/features/auth/model/schema.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
+import { userSchema } from "@/entities/user/model/schemas";
 
-export const loginSchema = z.object({
-  email: z.email(),
-  password: z.string(),
-});
+export const loginSchema = userSchema
+  .pick({
+    email: true,
+  })
+  .extend({
+    password: z.string(),
+  });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
