81fccc31ca68c71d7e1c3fa3a26803b38b4854dc
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 12:57:59 +0100

    test(api): add test for CSRF cookie retrieval

--- apps/api/tests/Feature/ExampleTest.php
+++ apps/api/tests/Feature/ExampleTest.php
@@ -1,3 +1,5 @@
 <?php
 
 it('returns a successful response', fn ()=> $this->get('/api')->assertSuccessful());
+
+it('returns the csrf cookie', fn ()=> $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN'));
