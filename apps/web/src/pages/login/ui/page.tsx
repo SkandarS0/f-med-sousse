25bec7b0d299c13df23e5afdb7ad047d0fa8aa79
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 23:13:42 +0100

    refactor(web/fsd): move login feature folder to auth

--- apps/web/src/pages/login/ui/page.tsx
+++ apps/web/src/pages/login/ui/page.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { LoginForm } from "@/features/login/ui/form";
+import { LoginForm } from "@/features/auth/ui/form";
 import {
   Card,
   CardContent,
