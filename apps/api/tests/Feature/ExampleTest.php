29eaa6487dc250fc033d160836aaec01080376c4
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 13:04:16 +0100

    feat(tests): add JSON request test for unauthenticated user access to user endpoint

--- apps/api/tests/Feature/ExampleTest.php
+++ apps/api/tests/Feature/ExampleTest.php
@@ -1,7 +1,11 @@
 <?php
 
-use function Pest\Laravel\{get};
+use function Pest\Laravel\{get, getJson};
 
 it('returns a successful response', fn ()=> get('/api')->assertSuccessful());
 
 it('returns the csrf cookie', fn ()=> get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN'));
+
+test('unauthenticated user cannot access user endpoint', function () {
+    getJson('/api/user')->assertUnauthorized();
+});
