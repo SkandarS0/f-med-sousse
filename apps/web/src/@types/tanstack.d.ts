89894d463f3b745619c2343d93e2bb4fc6aa092f
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 16:45:22 +0100

    feat(types): rename tanstack-router.d.ts to tanstack.d.ts and add tanstack query defaultError type to ApiErrorResponse

--- apps/web/src/@types/tanstack.d.ts
+++ apps/web/src/@types/tanstack.d.ts
@@ -1,7 +1,14 @@
+import type { ApiErrorResponse } from "@/shared/api/axios";
 import type { routerInstance } from "@/shared/config/tanstack-router";
 
 declare module "@tanstack/react-router" {
   interface Register {
     router: ReturnType<typeof routerInstance>;
+  }
+}
+
+declare module "@tanstack/react-query" {
+  interface Register {
+    defaultError: ApiErrorResponse;
   }
 }
