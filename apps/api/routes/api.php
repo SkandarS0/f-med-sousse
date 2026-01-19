6d068935eedd21b536b6c70a281cc1772af81026
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 01:18:09 +0100

    chore(api): refactor route definitions to use arrow functions for brevity

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -3,11 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/user', fn (Request $request) => $request->user());
 
 
-Route::get('/', function (Request $request) {
-    return response()->json(['message' => 'Oops! You have reached the API root endpoint. Don\'t tell anyone!']);
-});
+Route::get('/', fn () => response()->json(['message' => 'Oops! You have reached the API root endpoint. Don\'t tell anyone!']));
