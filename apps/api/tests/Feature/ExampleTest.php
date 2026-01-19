51ae230c0c88b55085ee32ddef49225a1535ee26
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-19 13:04:21 +0100

    feat(tests): update tests to use getJson for API requests

--- apps/api/tests/Feature/ExampleTest.php
+++ apps/api/tests/Feature/ExampleTest.php
@@ -1,10 +1,10 @@
 <?php
 
-use function Pest\Laravel\{get, getJson};
+use function Pest\Laravel\{getJson};
 
-it('returns a successful response', fn ()=> get('/api')->assertSuccessful());
+it('returns a successful response', fn ()=> getJson('/api')->assertSuccessful());
 
-it('returns the csrf cookie', fn ()=> get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN'));
+it('returns the csrf cookie', fn ()=> getJson('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN'));
 
 test('unauthenticated user cannot access user endpoint', function () {
     getJson('/api/user')->assertUnauthorized();
