f20f9052fd9e14aa962d219987dee193b0bc8574
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-20 21:08:35 +0100

    feat(auth): enhance portal route with user authentication checks

--- apps/web/src/app/routes/portal/route.tsx
+++ apps/web/src/app/routes/portal/route.tsx
@@ -1,7 +1,13 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getUserQueryOptions } from "@/features/auth/api/get-user";
 
 export const Route = createFileRoute("/portal")({
-  beforeLoad(ctx) {
-    console.log("Authenticated route beforeLoad", ctx);
+  beforeLoad: async ({ context, location }) => {
+    await context.queryClient.ensureQueryData(getUserQueryOptions).catch(() => {
+      throw redirect({ to: "/login", search: { redirectTo: location.href } });
+    });
   },
+  loader: async ({ context }) => ({
+    user: await context.queryClient.ensureQueryData(getUserQueryOptions),
+  }),
 });
