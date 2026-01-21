756ae1d812c006f5c806ccd28c27ba73b2084c9c
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-21 17:21:01 +0100

    refactor(title): remove unused i18n from useRouteTitle and used string interpolation instead of concatenation

--- apps/web/src/shared/routes/title.ts
+++ apps/web/src/shared/routes/title.ts
@@ -6,10 +6,10 @@ import type { FileRoutesByTo } from "./routeTree.gen";
 type Routes = keyof FileRoutesByTo;
 
 function useRouteTitle(routeName: Routes) {
-  const { t, i18n } = useTranslation("routes");
+  const { t } = useTranslation("routes");
   const title = useMemo(() => {
-    return t(`${routeName}.title`) + ` | ${appConfig.name}`;
-  }, [routeName, t, i18n.language]);
+    return `${t(`${routeName}.title`)} | ${appConfig.name}`;
+  }, [routeName, t]);
   return title;
 }
 
