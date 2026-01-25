1408512aeccda7885716f029bc208afc4820d049
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-25 19:43:18 +0100

    fix(api/user): remove cookie setting for user preferred locale in response

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return response()->json($request->user())->withCookie(Cookie::make(config('app.locale_cookie_name'), $request->user()->preferredLocale()));
+    return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/', fn () => response()->json(['message' => 'Oops! You have reached the API root endpoint. Don\'t tell anyone!']));
