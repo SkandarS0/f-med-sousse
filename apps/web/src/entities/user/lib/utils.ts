9fdfe55cb41263089013e6e847c7d9ebab5b2148
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-26 02:10:14 +0100

    feat(user): add utility functions for user initials and full name

---
+++ apps/web/src/entities/user/lib/utils.ts
@@ -0,0 +1,23 @@
+import type { SupportedLng } from "@/shared/i18n/i18next";
+
+export const getUserInitials = (
+  firstName: string,
+  lastName: string,
+  language?: SupportedLng,
+) => {
+  const firstInitial = firstName ? firstName.charAt(0).toUpperCase() : "";
+  const lastInitial = lastName ? lastName.charAt(0).toUpperCase() : "";
+  return language === "fr"
+    ? `${lastInitial}${firstInitial}`
+    : `${firstInitial}${lastInitial}`;
+};
+
+export const getUserFullName = (
+  firstName: string,
+  lastName: string,
+  language?: SupportedLng,
+) => {
+  return language === "fr"
+    ? `${lastName} ${firstName}`
+    : `${firstName} ${lastName}`;
+};
