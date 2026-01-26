a5abef47d0fefe17a3743fc7260899e83bfc43aa
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-26 02:05:59 +0100

    Revert "feat(user): add full name attribute and update user schemas"
    This reverts commit dea9c381533cd4f8206d4e9219c3b648b1b60b9b.

--- apps/web/src/entities/user/model/dto.ts
+++ apps/web/src/entities/user/model/dto.ts
@@ -1,22 +1,20 @@
-import type z from "zod";
+import z from "zod";
 import { apiUserSchema } from "../api/dto";
 
 export const userSchema = apiUserSchema
   .omit({
     first_name: true,
     last_name: true,
-    full_name: true,
     email_verified_at: true,
     created_at: true,
     updated_at: true,
   })
   .extend({
-    firstName: apiUserSchema.shape.first_name,
-    lastName: apiUserSchema.shape.last_name,
-    fullName: apiUserSchema.shape.full_name,
-    emailVerifiedAt: apiUserSchema.shape.email_verified_at,
-    createdAt: apiUserSchema.shape.created_at,
-    updatedAt: apiUserSchema.shape.updated_at,
+    firstName: z.string(),
+    lastName: z.string(),
+    emailVerifiedAt: z.string().nullable(),
+    createdAt: z.string(),
+    updatedAt: z.string().nullable(),
   });
 
 export type User = z.infer<typeof userSchema>;
