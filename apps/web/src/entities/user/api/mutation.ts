86ec827ce7f1c21b4513216be2851e856c5fd3b1
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-27 01:50:06 +0100

    refactor(auth): move login mutation to user API and remove redundant file

--- apps/web/src/entities/user/api/mutation.ts
+++ apps/web/src/entities/user/api/mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { userQueries } from "@/entities/user/api/query";
 import { api, getCsrfCookie } from "@/shared/api/axios";
+import type { LoginFormSchema } from "../model/schemas";
 
 export function useAuthLogout() {
   return useMutation({
@@ -14,4 +15,21 @@ export function useAuthLogout() {
 async function logoutRequest() {
   await getCsrfCookie();
   return await api.post("/api/auth/logout");
+}
+type LoginRequestBody = LoginFormSchema;
+
+type LoginResponseBody = { two_factor: boolean };
+
+export function useAuthLogin() {
+  return useMutation({
+    mutationFn: (data: LoginFormSchema) => loginRequest(data),
+    onSuccess(_data, _variables, _onMutateResult, context) {
+      context.client.refetchQueries(userQueries.xGet());
+    },
+  });
+}
+
+async function loginRequest(data: LoginRequestBody) {
+  await getCsrfCookie();
+  return await api.post<LoginResponseBody>("/api/auth/login", data);
 }
