ec4a50c845c87d59c5ba99e35d61778637a33a38
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-29 00:55:27 +0100

    fix(web/routes): correct import statement for zod in the forgot-password route

--- apps/web/src/app/routes/(guest)/forgot-password.tsx
+++ apps/web/src/app/routes/(guest)/forgot-password.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { z } from "zod";
+import z from "zod";
 import { ForgotPasswordPage } from "@/pages";
 import { useRouteTitle } from "@/shared/routes/title";
 
