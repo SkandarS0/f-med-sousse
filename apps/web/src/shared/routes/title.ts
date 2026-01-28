8c426c019add3980203ed07031687e44c100b0d6
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-28 14:26:25 +0100

    refactor(web/config): move appConfig from index.ts to app.ts

--- apps/web/src/shared/routes/title.ts
+++ apps/web/src/shared/routes/title.ts
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { appConfig } from "../config";
+import { appConfig } from "../config/app";
 import type { FileRoutesByTo } from "./routeTree.gen";
 
 type Routes = keyof FileRoutesByTo;
