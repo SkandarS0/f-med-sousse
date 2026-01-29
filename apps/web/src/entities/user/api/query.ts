5b7d781e931d67f281ac24bdfc04bfcd4b00fd5e
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-29 04:33:08 +0100

    fix(user-queries): set retry option to 1 for user query

--- apps/web/src/entities/user/api/query.ts
+++ apps/web/src/entities/user/api/query.ts
@@ -13,7 +13,7 @@ export const userQueries = {
         return response.data;
       },
       select: (data) => userMapper.fromApi(data),
-      retry: false,
+      retry: 1,
       staleTime: 5 * 60 * 1000, // 5 minutes
       refetchOnWindowFocus: false,
       throwOnError: false,
