8aead5b3d02234a9e694f3c89e1e2a5b2da95de0
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-30 21:38:25 +0100

    refactor(web): inline the title variable

--- apps/web/src/shared/routes/title.ts
+++ apps/web/src/shared/routes/title.ts
@@ -7,10 +7,9 @@ type Routes = keyof FileRoutesByTo;
 
 function useRouteTitle(routeName: Routes) {
   const { t } = useTranslation("routes");
-  const title = useMemo(() => {
+  return useMemo(() => {
     return `${t(`${routeName}.title`)} | ${appConfig.name}`;
   }, [routeName, t]);
-  return title;
 }
 
 export { useRouteTitle };
