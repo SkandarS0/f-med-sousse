005fc491d395836fcb38651fae555d0e8527952d
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-01-31 15:12:29 +0100

    refactor(api/tests): replace `actingAs` with `Sanctum::actingAs` and remove obsolete login test

--- apps/api/tests/Feature/ExampleTest.php
+++ apps/api/tests/Feature/ExampleTest.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\Sanctum;
 
-use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
-use function Pest\Laravel\postJson;
 
 it('returns a successful response', fn () => getJson('/api')->assertSuccessful());
 
@@ -18,29 +16,11 @@ test('unauthenticated user cannot access user endpoint', function () {
 test('authenticated user can access user endpoint', function () {
     $user = User::factory()->create();
 
-    actingAs($user);
+    Sanctum::actingAs($user);
 
     getJson('/api/user')
         ->assertOk()
         ->assertJson([
             'email' => $user->email,
         ]);
-});
-
-test('a user can login with correct credentials with fortify', function () {
-    $password = 'password123';
-    $user = User::factory()->create([
-        'password' => Hash::make($password),
-    ]);
-
-    $csrfResponse = getJson('/sanctum/csrf-cookie');
-    $csrfResponse->assertCookie('XSRF-TOKEN');
-    $csrfToken = $csrfResponse->getCookie('XSRF-TOKEN')->getValue();
-    $response = postJson('/auth/login', [
-        'email' => $user->email,
-        'password' => $password,
-    ], [
-        'X-CSRF-TOKEN' => $csrfToken,
-    ]);
-    $response->assertOk();
 });
