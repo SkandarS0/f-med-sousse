ae410c1774c8ea3094c195be6d9c086cf02beb41
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-02-02 14:55:23 +0100

    fix(api/routes): apply `auth:sanctum` middleware directly to user route group

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use App\Http\Controllers\Web\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'user'], function (): void {
+Route::group(['prefix' => 'user', 'middleware' => 'auth:sanctum'], function (): void {
     Route::get('/', [UserController::class, 'index'])->name('user.index');
-})->middleware('auth:sanctum');
+});
 
 Route::get('/', fn () => response()->json(['message' => 'Oops! You have reached the API root endpoint. Don\'t tell anyone!']));
