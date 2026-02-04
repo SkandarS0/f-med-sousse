c2a00d4c797fa0fc8e7ee171232c59779e2b50c9
Skandar Souissi <iskandarsouissi.0@gmail.com>
2026-02-04 17:03:11 +0100

    chore(seed): extract user seeding logic into `UsersSeeder`

--- apps/api/database/seeders/DatabaseSeeder.php
+++ apps/api/database/seeders/DatabaseSeeder.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 final class DatabaseSeeder extends Seeder
 {
@@ -15,9 +13,6 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->admin()->create([
-            'email' => 'test@example.com',
-            'password' => Hash::make('142536'),
-        ]);
+        $this->call([UsersSeeder::class]);
     }
 }
